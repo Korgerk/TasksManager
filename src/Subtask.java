1d44aa53e4653bc58aa28ba82c9a690a58d4d68d
Nohym <afer.as@mail.ru>
2025-04-04 15:19:42 +0300

    Initial commit

---
+++ src/Subtask.java
@@ -0,0 +1,28 @@
+public class Subtask extends Task {
+    private final int epicID;
+
+    public Subtask(String name, String description, int epicID) { // c новым
+        super(name, description);
+        this.epicID = epicID;
+    }
+
+    public Subtask(int id, String name, String description, Status status, int epicID) { // со всеми
+        super(id, name, description, status);
+        this.epicID = epicID;
+    }
+
+    public int getEpicID() {
+        return epicID;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", id=" + getId() +
+                ", epicID=" + epicID +
+                ", status=" + getStatus() +
+                '}';
+    }
+}
